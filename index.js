3261e358565c0251b47a013865ba39870c2d40ba
Joshua Franklin <mail@joshfrankl.in>
2018-12-01 13:07:44 -0700

    Added flying cat.

---
+++ index.js
@@ -0,0 +1,32 @@
+const cat = document.getElementById('flying-cat');
+const grav = 0.2;
+
+let x = 0;
+let y = 0;
+let vel_x = 10;
+let vel_y = 0;
+
+function update() {
+    vel_y += grav;
+
+    y += vel_y;
+    x += vel_x;
+
+    if (x + 200 >= window.innerWidth || x <= 0) {
+        vel_x = -vel_x;
+    }
+    if (y + 200 >= window.innerHeight) {
+        vel_y = -(vel_y * 0.9);
+        y -= 2;
+    }
+
+    cat.style.left = x + 'px';
+    cat.style.top = y + 'px';
+}
+
+function jump() {
+    vel_y = -10;
+}
+
+window.addEventListener('click', jump);
+setInterval(update, 20);
