1b0aecc125aae2cdcd26121a83d67ebdc4188300
Tiffany Phan <Tiffany.Phan@colorado.edu>
2018-12-01 10:59:55 -0700

    finalized ad loop

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,8 @@
 const ad_list = document.querySelectorAll('iframe[title="Ad"]'); // Selects all ads on the page.
 
-ad_list.forEach(function(element) {
+ad_list.forEach(function(element) {	// For each ad element,
+	let width = element.width;
+	let height = element.height;
 
+	element.src = 'https://placekitten.com/' + width + '/'  + height;
 });
