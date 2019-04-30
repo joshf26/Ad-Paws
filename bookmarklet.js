66b8c9bdeb0eb9153fba45d51fd37dfc733f57a2
Tiffany Phan <Tiffany.Phan@colorado.edu>
2019-04-30 15:09:25 -0600

    Supports chrome extension

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,5 @@
-document.querySelectorAll('iframe').forEach(function(element) {
-	element.src = 'https://joshf26.github.io/Ad-Paws/ad-replacement.html';
-});
+setTimeout(() => {
+	document.querySelectorAll('iframe').forEach(function(element) {
+		element.src = 'https://joshf26.github.io/Ad-Paws/ad-replacement.html';
+	});
+}, 2500);
